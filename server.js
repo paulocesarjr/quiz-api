9e17699daad40b680e0321e723df92f7b3954929
paulocesarjr <paulo.rosa@code7.com>
2022-07-05 16:08:02 -0300

    Ajuste de porta.

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const server = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 server.use(bodyParser.json());
 
 // importação de rotas aqui...
 
-server.listen(port, () => {
-    console.log(`API do quiz rodando na porta: ${port}`);
+server.listen(PORT, () => {
+    console.log(`API do quiz rodando na porta: ${PORT}`);
 });
 
 
